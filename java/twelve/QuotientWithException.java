c3236ffbd65acdb3fab0bb4b706d86a957f501b1
aurorg <2519652258@qq.com>
2022-07-12 10:56:03 +0800

    1

--- java/twelve/QuotientWithException.java
+++ java/twelve/QuotientWithException.java
@@ -1,11 +1,13 @@
 import java.util.Scanner;
 public class QuotientWithException {
     public static int quotient(int number1, int number2){
+
         if(number2==0)
             throw new ArithmeticException("Divisor cannot be zero");
 
         return number1/number2;
     }
+
     public static void main(String[] args){
         Scanner input =new Scanner(System.in);
 
