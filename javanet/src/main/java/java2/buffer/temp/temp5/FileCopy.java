bf46741a721603363dc5373cdb48d5bcd94e6822
aurorg <2519652258@qq.com>
2022-07-15 16:31:38 +0800

    1

--- javanet/src/main/java/java2/buffer/temp/temp5/FileCopy.java
+++ javanet/src/main/java/java2/buffer/temp/temp5/FileCopy.java
@@ -11,11 +11,13 @@ public class FileCopy {
     public static void main(String[] args) throws IOException {
         FileInputStream in = new FileInputStream("/home/shizhanli/szl/happy.txt");
         FileOutputStream out = new FileOutputStream("/home/shizhanli/szl/happycopy.txt");
-        FileChannel src = in.getChannel();
-        FileChannel dst = out.getChannel();
+
+        FileChannel src = in.getChannel();//原来的文件
+        FileChannel dst = out.getChannel();//复制后的文件
 
         src.transferTo(0,src.size(),dst);
         // 或dst.transferFrom(src,0,src.size());
+
 
         in.close();
         out.close();
