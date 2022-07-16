503e553b96d8a626519783337d56c3094d23371a
aurorg <2519652258@qq.com>
2022-07-16 16:51:44 +0800

    1

--- javanet/src/main/java/java2/buffer/test/test6/NIOClient.java
+++ javanet/src/main/java/java2/buffer/test/test6/NIOClient.java
@@ -12,7 +12,7 @@ public class NIOClient {
         SocketChannel socketChannel=SocketChannel.open();
 
         //2.设置连接IP和端口号
-        socketChannel.connect(new InetSocketAddress("129.0.0.1",9999));
+        socketChannel.connect(new InetSocketAddress("127.0.0.1",9999));
 
         //3.写数据
         socketChannel.write(ByteBuffer.wrap("老板，该还钱了".getBytes(StandardCharsets.UTF_8)));
@@ -20,6 +20,11 @@ public class NIOClient {
         //4.读取服务器写回的数据
         ByteBuffer readBuffer =ByteBuffer.allocate(1024);
         int read =socketChannel.read(readBuffer);
+
+        //String(byte[] bytes, int offset, int length, Charset charset)
+        //构造一个新的String通过使用指定的指定字节子阵列解码charset 。
+        //UTF_8万国码的升级版
+
         System.out.println("服务器消息：" + new String(readBuffer.array(),0,read,StandardCharsets.UTF_8));
 
         //5.释放资源
