fdf23878ac0026e5b93dbdea8ac813167f554af9
aurorg <2519652258@qq.com>
2022-07-19 20:36:30 +0800

    1

--- javanet/src/main/java/java3netty/temp/temp3/NettyChatServerHandler.java
+++ javanet/src/main/java/java3netty/temp/temp3/NettyChatServerHandler.java
@@ -1,8 +1,24 @@
 package java3netty.temp.temp3;
 
+import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 
-public class NettyChatServerHandler {
+import java.util.ArrayList;
+import java.util.List;
+
+public class NettyChatServerHandler extends SimpleChannelInboundHandler<String>{
+    public static List<Channel>  channelList = new ArrayList<>();
+    
+    //管道就绪事件
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+         Channel channel =ctx.channel();
+
+         //当有新的客户端连接的时候，将通道放入集合
+        channelList.add(channel);
+        System.out.println("[Server] :" + channel.remoteAddress().toString().substring(1) + "在线。");
+    }
+
 
 }
