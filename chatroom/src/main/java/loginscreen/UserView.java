53873ff3d15bf7218205dbef76febcad73aec7f3
aurorg <2519652258@qq.com>
2022-07-20 20:20:15 +0800

    1

--- chatroom/src/main/java/loginscreen/UserView.java
+++ chatroom/src/main/java/loginscreen/UserView.java
@@ -5,7 +5,9 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.Scanner;
 
-public class Test1 {
+//用户界面
+
+public class UserView {
     // MySQL 8.0 以上版本 - JDBC 驱动名及数据库 URL
     static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
     static final String DB_URL = "jdbc:mysql://localhost:3306/happy?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
@@ -28,7 +30,19 @@ public class Test1 {
     static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args){
+        /**
+         * 1.进行用户登录
+         * 先取数据库中找这个用户是否存在，如果存在的话，继续进行下一步操作；
+         * 如果这个用户不存在的话，调用用户注册函数，注册一个账号，之后再进行后续的操作。
+         */
+        //用户登录
+        loginview();
 
+
+    }
+
+    //用户登录界面
+    public static void loginview() {
         System.out.println("*******************************");
         System.out.println("*        欢迎来到用户界面        *");
         System.out.println("*       根据您的需求进行选择      *");
@@ -37,16 +51,45 @@ public class Test1 {
         System.out.println("*         [3]:用户注销          *");
         System.out.println("*******************************");
 
-        int n =input.nextInt();
-        switch(n){
+        int n = input.nextInt();
+        switch (n) {
             case 1:
-
+                enroll();
                 break;
             case 2:
-
+                login();
+                break;
+            case 3:
+                logout();
                 break;
         }
+    }
+
+    //用户登录函数
+    public static void enroll(){
+
+        System.out.println("请输入您的账号名称：");
+        String name=input.nextLine();
+
+        System.out.println("请输入您的密码：");
+        String password1 =input.nextLine();
+
+
+
+
 
     }
+
+    //用户注册函数
+    public static void login(){
+
+    }
+
+    //用户注销
+    public static void logout(){
+
+    }
+
+    
 
 }
