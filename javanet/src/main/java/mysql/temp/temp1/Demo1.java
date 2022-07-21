d44679ba89ba744f698daab7832d2e771052dc15
aurorg <2519652258@qq.com>
2022-07-21 15:24:49 +0800

    1

--- javanet/src/main/java/mysql/temp/temp1/Demo1.java
+++ javanet/src/main/java/mysql/temp/temp1/Demo1.java
@@ -1,6 +1,7 @@
 package mysql.temp.temp1;
 
 import java.sql.*;
+import java.util.Scanner;
 
 
 public class Demo1 {
@@ -22,12 +23,17 @@ public class Demo1 {
     //sql语句的执行结果
     static ResultSet rs = null;
 
+    //记录语句的输入
+    static PreparedStatement ps =null;
+
+
+    static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args)  {
 
         //1、测试read函数，读取数据库中的内容
-        read();
-//          del();
+        //read();
+          //del();
 
         //2、测试del函数，删除数据库中的数据
 //        try {
@@ -38,21 +44,21 @@ public class Demo1 {
 
         //3、测试添加函数
 //        try {
-//            add();
+ //          add();
 //        } catch (Exception e) {
 //            e.printStackTrace();
 //        }
 
         //4、测试修改函数
 //        try {
-//            update();
+             update();
 //        } catch (Exception e) {
 //            e.printStackTrace();
 //        }
 
     }
 
-
+//-——————————————————————————————————————————————————————————————————————————————————————————
     //读取数据
     public static void read(){
 
@@ -127,7 +133,7 @@ public class Demo1 {
         System.out.println("Goodbye!");
     }
 
-
+//——————————————————————————————————————————————————————————————————————————————————————————
     //删除
     public  static void del(){
         try{
@@ -137,11 +143,18 @@ public class Demo1 {
             //创建向数据库发送sql的statement对象
             stat = conn.createStatement();
 
+           String sql ="delete from pet where name=?";
+            ps =conn.prepareStatement(sql);
+
+            System.out.println("请输入要删除的：");
+            String s1 =input.next();
+            ps.setString(1,s1);
+
             //删除的mysql语句，然后进行删除
             //count用来返回相应的行数
-            int count = stat.executeUpdate("delete from pet where name= 'lalala'");
-            //executeUpdate(String sql)：用于向数据库发送insert、update或delete语句
+            int count = ps.executeUpdate();
 
+            //executeUpdate(String sql)：用于向数据库发送insert、update或delete语句
             if(count>0){
                 System.out.println("删除成功，受到影响的行数为："+count);
             }else{
@@ -150,6 +163,7 @@ public class Demo1 {
 
             //关闭资源
             //rs.close();
+            ps.close();
             stat.close();
             conn.close();
 
@@ -176,64 +190,115 @@ public class Demo1 {
     }
 
 
-
+//——————————————————————————————————————————————————————————————————————————————————————————
 
     //添加一条新的数据
-    public  static void add() throws SQLException,Exception{
+    public  static void add(){
         try{
             conn = DriverManager.getConnection(DB_URL, USER, PASS);
             stat = conn.createStatement();
 
-            //利用传输器传输数据
-            int count = stat.executeUpdate("insert into pet values('mimi1','xiaozhang','cat','f','1111-11-11',null)");
-            //executeUpdate(String sql)：用于向数据库发送insert、update或delete语句
+            String sql ="insert into pet(owner,name,birth) values(?,?,?) ";
+            ps=conn.prepareStatement(sql);
 
-            //返回相应的行数
+            System.out.println("请输入owner：");
+            String s1=input.next();
+            ps.setString(1,s1);
+
+            System.out.println("请输入name：");
+            String s2=input.next();
+            ps.setString(2,s2);
+
+            System.out.println("请输入birth：");
+            String s3=input.next();
+            ps.setString(3,s3);
+
+            int count =ps.executeUpdate();//返回相应的行数
             if(count > 0){
                 System.out.println("添加成功，受到影响的行数为："+count);
             }else{
                 System.out.println("添加失败");
             }
 
-        }catch(Exception e){
-            e.printStackTrace();
-            throw new RuntimeException(e);
-        }finally{
-            //6.关闭资源
-            //后创建的先关闭
-            //rs.close();
+            //关闭
+            ps.close();
             stat.close();
             conn.close();
+
+        } catch (SQLException se) {
+            // 处理 JDBC 错误
+            se.printStackTrace();
+        } catch (Exception e) {
+            // 处理 Class.forName 错误
+            e.printStackTrace();
+        } finally {
+            // 关闭资源
+
+            try {
+                if (stat != null) stat.close();
+            } catch (SQLException se2) {
+            }// 什么都不做
+
+            try {
+                if (conn != null) conn.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
         }
 
     }
 
+//——————————————————————————————————————————————————————————————————————————————————————————
 
     //更新数据，也就是修改
-    public static void update() throws SQLException,Exception{
+    public static void update(){
         try{
 
             conn = DriverManager.getConnection(DB_URL, USER, PASS);
             stat = conn.createStatement();
 
-            //4.利用传输器传输数据，并返响应行数。
-            int count = stat.executeUpdate("update pet set owner = 'nana' where name = 'aoliao'");
-            //executeUpdate(String sql)：用于向数据库发送insert、update或delete语句
+            String sql ="update pet set owner =? where name =?";
+            ps=conn.prepareStatement(sql);
 
+            System.out.println("请输入需要修改的owner：");
+            String s1=input.next();
+            ps.setString(1,s1);
+
+            System.out.println("请输入需要修改的name：");
+            String s2=input.next();
+            ps.setString(2,s2);
+
+//            //4.利用传输器传输数据，并返响应行数。
+//            int count = stat.executeUpdate("update pet set owner = 'nana' where name = 'aoliao'");
+//            //executeUpdate(String sql)：用于向数据库发送insert、update或delete语句
+
+
+
+            int count =ps.executeUpdate();
             if(count > 0){
                 System.out.println("修改成功，受到影响的行数为："+count);
             }else{
                 System.out.println("修改失败");
             }
-        }catch(Exception e){
-            e.printStackTrace();
-            throw new RuntimeException(e);
-        }finally{
-            //6.关闭资源
-            //后创建的先关闭
-            //rs.close();
+            ps.close();
             stat.close();
             conn.close();
+        } catch (Exception e) {
+            // 处理 Class.forName 错误
+            e.printStackTrace();
+        } finally {
+            // 关闭资源
+
+            try {
+                if (stat != null) stat.close();
+            } catch (SQLException se2) {
+            }// 什么都不做
+
+            try {
+                if (conn != null) conn.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
         }
 
     }
