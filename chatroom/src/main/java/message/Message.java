568a902aad6b3c867ed14e3558684a5a4bbb21b3
aurorg <2519652258@qq.com>
2022-07-22 15:30:50 +0800

    1

---
+++ chatroom/src/main/java/message/Message.java
@@ -0,0 +1,22 @@
+package message;
+
+public class Message {
+    private int msgtype;
+
+
+    //第一部分：注册，登录，注销，退出界面
+
+    private static final int enrollmsg =0;   //【注册】    客户端发给服务端的消息
+    private static final int enrollmsg1 =1;  //【注册】    服务端向客户端发的消息
+
+    private static final int loginmsg =3;    //【登录】    客户端发给服务端的消息
+    private static final int loginmsg1 =4;   //【登录】    服务端向客户端发的消息
+
+    private static final int logoutmsg =5;   //【注销】    客户端发给服务端的消息
+    private static final int logoumsg1 =6;   //【注销】    服务端向客户端发的消息
+
+    private static final int quit = 7;        //【退出系统】    客户端发给服务端的消息
+    private static final int quit1 =8;        //【退出系统】    服务端向客户端发的消息
+
+
+}
