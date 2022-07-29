5e9482069d750d55c62b9b7e9e490c7698b90b8c
aurorg <2519652258@qq.com>
2022-07-29 11:41:36 +0800

    1

--- chatroom/src/main/java/message/FriendGetFilemsg.java
+++ chatroom/src/main/java/message/FriendGetFilemsg.java
@@ -1,4 +1,33 @@
 package message;
 
-public class FriendGetFilemsg {
+public class FriendGetFilemsg extends Message {
+    private int userid;
+    private int friendid;
+    private boolean refuse=false;
+    public FriendGetFilemsg(){}
+    public FriendGetFilemsg(int userid,int Friendid){
+        this.userid=userid;
+        this.friendid=Friendid;
+    }
+
+    public void setRefuse(boolean refuse) {
+        this.refuse = refuse;
+    }
+
+    public boolean isRefuse() {
+        return refuse;
+    }
+
+    public int getUserid() {
+        return userid;
+    }
+
+    public int getFriendid() {
+        return friendid;
+    }
+
+    public String toString(){
+        return "userid = "+userid+", friendid = "+friendid;
+    }
+
 }
