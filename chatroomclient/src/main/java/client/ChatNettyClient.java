38125c7d5bf5a83b72679cf46b76071d47ec998d
aurorg <2519652258@qq.com>
2022-08-05 10:26:37 +0800

    1

---
+++ chatroomclient/src/main/java/client/ChatNettyClient.java
@@ -0,0 +1,106 @@
+package client;
+
+import client.clienthandler.CDengLuViewHandler;
+import common.MessageCodec;
+import common.ProtocolFrameDecoder;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+import java.io.File;
+import java.util.List;
+import java.util.Map;
+
+public class ChatNettyClient {
+
+    public static final Object waitMessage=new Object();//服务端消息返回时，notify线程去通知
+    public static final Object waitfile=new Object();
+
+    public static volatile int waitSuccess=0;//1表示消息成功、0表示消息失败
+
+    public static volatile boolean unRead1=false;//默认没有未读好友消息
+    public static volatile boolean unRead2=false;//默认没有未读群消息
+
+    public static volatile boolean is1=false; //用来判断好友发消息的
+    public static volatile boolean is2=false; //用来判断群发消息的
+
+    public static volatile int talker=0; //聊天的人（发文件用）
+    public static volatile String check="n";//no
+    public static volatile boolean isyes=false;
+    public static volatile String havefile="";
+
+
+    public static volatile Map<String, List<String>> informationMap;//
+
+    public static volatile List<String> friendmsglist;//查询好友历史消息列表
+    public static volatile List<String> friendmsglist1;//查询好友未读消息列表
+    public static volatile List<String> friendlist; //查询好友列表的
+    public static volatile File FriendGetFilemsg; //好友保存文件到本地的
+    public static volatile File GroupGetFilemsg;  //群聊保存文件到本地的
+
+    public static volatile List<String> grouplist; //查询好友列表的
+    public static volatile List<String> grouphistorymsg; //查询群历史消息
+    public static volatile List<String> groupunreadmsg;//查询群未读消息的
+
+    public static volatile int chatting=0;
+
+
+    public static void main(String[] args) throws Exception{
+
+        //客户端需要一个事件循环组
+        NioEventLoopGroup group = new NioEventLoopGroup();
+
+        LoggingHandler Log=new LoggingHandler(LogLevel.DEBUG);
+      //  MessageCodec clientCodec=new MessageCodec();
+
+
+
+        try {
+            //创建客户端启动对象
+            //注意客户端使用的不是ServerBootstrap,而是Bootstrap
+            Bootstrap bootstrap = new Bootstrap();
+
+            bootstrap.group(group) //设置线程组
+                    .channel(NioSocketChannel.class) //设置客户端通道的实现类
+                    .handler( new ChannelInitializer<NioSocketChannel>() {
+                        @Override
+                        protected void initChannel(NioSocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(new ProtocolFrameDecoder());
+                            ch.pipeline().addLast(new MessageCodec()); //解码编码的
+                            ch.pipeline().addLast(new ResponseHandler()); //服务端给客户端回消息的处理器
+                            ch.pipeline().addLast(new CFriendChatHandler());//好友聊天的
+                            ch.pipeline().addLast(new CGroupChatHandler()); //群聊的
+                            //ch.pipeline().addLast(new ServerToClientmsg());
+                            ch.pipeline().addLast("CLoginViewHandler",new ChannelInboundHandlerAdapter(){  //加入自己的处理器,需要什么处理器加什么处理器（这个后面都可以加）
+
+                                @Override
+                                public void channelActive(ChannelHandlerContext ctx) throws Exception {
+
+                                    //创建一个线程专门用来跑这些界面（登录界面，主界面）的界面层
+                                    new Thread(()->{new CDengLuViewHandler(ctx);},"system.in").start();
+                                }
+
+                            });
+                        }
+                    });
+
+            System.out.println("客户端 ok.....");
+
+
+            //启动客户端取连接服务端
+            //关于ChannelFuture需要分析：涉及netty的异步模型
+            ChannelFuture channelFuture = bootstrap.connect("127.0.0.1", 6686).sync();
+
+            //给关闭通道增加一个连接进行监听
+            channelFuture.channel().closeFuture().sync();
+        }finally{
+            group.shutdownGracefully();
+        }
+    }
+}
---
+++ chatroomserver/src/main/java/server/ChatNettyServer.java
@@ -0,0 +1,134 @@
+package server;
+
+import common.MessageCodec;
+import common.ProtocolFrameDecoder;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import server.serverhandler.*;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class ChatNettyServer {
+
+    public static void jdbcmysql(){
+        // MySQL 8.0 以上版本 - JDBC 驱动名及数据库 URL
+        final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+        final String DB_URL = "jdbc:mysql://localhost:3306/chatroom?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+
+        //数据库用户和密码
+        final String USER = "root";
+        final String PASS = "szl0905";
+
+        //数据的连接对象
+        Connection conn = null;
+
+        //传输器
+        Statement stat = null;
+
+        //sql语句的执行结果
+        ResultSet rs = null;
+
+        //记录语句的输入
+        PreparedStatement ps =null;
+    }
+
+    public static void main(String[] args) throws IOException ,InterruptedException{
+
+        //创建BossGroup和WorkGroup
+        //说明
+        //1.创建两个线程组bossGroup和workerGroup
+        //2.bossGroup只是处理连接请求，真正的和客户端业务处理，会交给workerGroup完成
+        //3.两个都是无限循环
+        //4.bossGroup和workerGroup含有的子线程【NioEventLoop】的个数
+        //  默认为：实际cpu核数 *2  【可以自己设置线程的数量】
+        jdbcmysql();
+
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup();
+
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        LoggingHandler Log=new LoggingHandler(LogLevel.DEBUG);
+
+
+        try {
+
+            //创建服务器端启动的对象，配置参数
+            ServerBootstrap bootstrap = new ServerBootstrap();
+
+            //使用链式编程来进行设置
+            bootstrap.group(bossGroup, workerGroup)  //设置两个线程组
+                    .channel(NioServerSocketChannel.class)  //使用NioSocketChannel作为服务器的通道实现
+                    //.option(ChannelOption.SO_BACKLOG, 128) //设置线程队列得到的连接数
+                    //.childOption(ChannelOption.SO_KEEPALIVE, true) //设置保持活动的连接状态
+                    .childHandler(new ChannelInitializer<NioSocketChannel>() { //创建一个通道测试对象（匿名对象）
+
+                        //给pipeline设置处理器
+                        @Override
+                        protected void initChannel(NioSocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(new ProtocolFrameDecoder());
+                            ch.pipeline().addLast(new MessageCodec());
+
+                            //需要用什么处理器直接加就行了
+                            ch.pipeline().addLast(new SEnrollViewHandle()); //注册
+                            ch.pipeline().addLast(new SLoginViewHandler());//登录
+                            ch.pipeline().addLast(new SLogoutmsgViewHandler());//注销
+                            ch.pipeline().addLast(new SOffLineHandler()); //下线处理
+
+                            ch.pipeline().addLast(new SFriendChatHandler()); //好友聊天
+                            ch.pipeline().addLast(new SInformationfriendhistoryHandler()); //查看好友历史消息
+                            ch.pipeline().addLast(new SInformationfriendunreadHandler());  //查看好友未读消息的
+                            ch.pipeline().addLast(new SSendApplyHandler()); //发送好友申请
+                            ch.pipeline().addLast(new SPassFriendApplyHandler()); //通过好友申请的
+                            ch.pipeline().addLast(new SDeleteFriendHandler());  //删除好友的
+                            ch.pipeline().addLast(new SShieldFriendHandler()); //屏蔽好友的
+                            ch.pipeline().addLast(new SFriendListHandler()); //查看好友列表的
+                            ch.pipeline().addLast(new SFriendGetFileHandler()); //接收文件的
+
+
+                            ch.pipeline().addLast(new SGroupAuthenticationMessage()); //群成员身份确认的
+                            ch.pipeline().addLast(new SGroupSetupHandler());//建群的
+                            ch.pipeline().addLast(new SGroupDeleteHandler());//解散群的
+                            ch.pipeline().addLast(new SGroupSendApplyHandler());//申请加群的
+                            ch.pipeline().addLast(new SGroupJoinHandler()); //处理加群通知的
+                            ch.pipeline().addLast(new SGroupQuitHandler()); //退群的
+                            ch.pipeline().addLast(new SGroupListMemberHandler()); //群成员列表
+                            ch.pipeline().addLast(new SGroupHistorymsgHandler()); //群历史消息
+                            ch.pipeline().addLast(new SGroupUnreadmsgHandler()) ; //群未读消息的
+                            ch.pipeline().addLast(new SGroupAddAdministratorHandler()); //添加管理员身份
+                            ch.pipeline().addLast(new SGroupDeleteAdministratorHandler()); //删除管理员身份
+                            ch.pipeline().addLast(new SGroupDeleteMemberHandler()) ; //将用户移出群
+                            ch.pipeline().addLast(new SGroupOpenBanSpeakHandler()) ; //群开启禁言模式
+                            ch.pipeline().addLast(new SGroupCloseBanSpeakHandler()); //群关闭禁言模式
+                            ch.pipeline().addLast(new SGroupChatHandler()); //群聊的
+                            ch.pipeline().addLast(new SGroupGetFileHandler()); //接受群文件的
+                            ch.pipeline().addLast(new SFindPasswordHandler()); //找回密码的
+                            ch.pipeline().addLast(new SUpdatePasswordHandler()); //修改密码的
+                        }
+
+                    }); //给workerGroup的EventLoop对应的管道设置处理器
+
+            System.out.println("服务器启动好了......");
+
+            //绑定一个端口并且同步，生成一个ChannelFuture对象
+            //启动服务器（并且绑定端口）
+            ChannelFuture cf = bootstrap.bind(6686).sync();
+
+            //对关闭通道进行监听
+            cf.channel().closeFuture().sync();
+
+        }finally{
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
